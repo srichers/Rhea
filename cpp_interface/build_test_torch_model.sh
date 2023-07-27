c407121e17e0407deea517885c2ee5d9d42f1cbd
Sherwood Richers <richers@utk.edu>
2023-07-27 01:09:03 -0400

    updated suggested build commands to match those executed by vscode

--- cpp_interface/build_test_torch_model.sh
+++ cpp_interface/build_test_torch_model.sh
@@ -1,4 +1,3 @@
-mkdir -p build
-cd build
-cmake ..
-cmake --build . --config Release
+/home/srichers/.local/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S/mnt/scratch/srichers/software/Rhea/cpp_interface -B/mnt/scratch/srichers/software/Rhea/build -G Ninja
+
+/home/srichers/.local/bin/cmake --build /mnt/scratch/srichers/software/Rhea/build --config Debug --target all --
